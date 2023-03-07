adf4fc639468472e2c1149d3bc376bcc43fd42e8
Daniel <denamudu@bariansystems.com>
2023-03-07 11:42:58 +0100

    Fixed the issues I created

--- public/js/ie8.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function(){
-	"use strict";
-    jQuery('body').html('<div class="ie8"><span>Hi! Please, use the latest version of the browser in order to visit current page</span></div>');
-});
